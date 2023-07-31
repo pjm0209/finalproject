b9e63f691f29dc542569f2752883bf3a12d72ac5
pjm0209 <137860775+pjm0209@users.noreply.github.com>
2023-07-31 16:25:24 +0900

    application 수정

--- src/main/java/com/team2/mbti/ServletInitializer.java
+++ src/main/java/com/team2/mbti/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(FinalTeamProjectApplication.class);
+		return application.sources(FinalProjectApplication.class);
 	}
 
 }
