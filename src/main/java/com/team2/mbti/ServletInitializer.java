0e8dc14e9abd7718324dd073ea616d0ef6355e84
pjm0209 <137860775+pjm0209@users.noreply.github.com>
2023-07-31 16:54:52 +0900

    FinalTeamProject로 다시 변경

--- src/main/java/com/team2/mbti/ServletInitializer.java
+++ src/main/java/com/team2/mbti/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(FinalProjectApplication.class);
+		return application.sources(FinalTeamProjectApplication.class);
 	}
 
 }
