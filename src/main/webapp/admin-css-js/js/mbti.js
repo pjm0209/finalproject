9d9eb66fc8f15c814285b4dd2eaa77cf210a6075
pjm0209 <137860775+pjm0209@users.noreply.github.com>
2023-08-03 17:36:21 +0900

    mbti 수정 by PJM

--- src/main/webapp/admin-css-js/js/mbti.js
+++ src/main/webapp/admin-css-js/js/mbti.js
@@ -9,5 +9,28 @@
     		const isChecked = $(this).prop('checked');
     		$('.check').prop('checked', isChecked);
   	    });
+  	    
+  	    $('#mbti-write-button').click(function(){
+			open(contextPath+"/admin/mbti/mbtiWrite",'mbtiWrite','width=800,height=800,top=50,left=500');
+		});
+  	    
+  	    $('#mbti-edit-button').click(function(){
+			var num=0;
+			$('input[type=checkbox]:checked').each(function(){
+				num++;
+				if(num>1){
+					alert('여러 개를 수정할 수 없습니다.');
+					event.defaultPrevented();
+					return false;
+				}
+			});
+			var result=$('input[type=checkbox]:checked').val();
+			
+			if(result==null){
+				alert('수정할 질문을 체크하세요');
+				return false;
+			}
+			open(contextPath+"/admin/mbti/mbtiWrite?mbtiServeyNo="+result,'mbtiWrite','width=800,height=800,top=50,left=500');
+		});
 		
 	});
