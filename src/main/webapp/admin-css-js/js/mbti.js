baf41b3b9d49fbf3c0e9fcc90263d3c24d735ed8
pjm0209 <137860775+pjm0209@users.noreply.github.com>
2023-08-09 18:16:02 +0900

    main mbti, main top mbti 경로 수정 by PJM

--- src/main/webapp/admin-css-js/js/mbti.js
+++ src/main/webapp/admin-css-js/js/mbti.js
@@ -40,4 +40,11 @@
 			}
 		});
 		
+		$('#mbti-free-button').click(function(){
+			location.href=contextPath+"/main/mbti/question?questionTypeNo=1";
+		});
+		
+		$('#mbti-precise-button').click(function(){
+			location.href=contextPath+"/main/mbti/question?questionTypeNo=2";
+		});
 	});
