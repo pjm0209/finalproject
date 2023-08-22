26bb9b421df87e939f61aecc79627da04c6569d7
happyplaid <kingdom1723@naver.com>
2023-08-22 10:48:23 +0900

    yjk

--- src/main/java/com/team2/mbti/book/model/BookServiceImpl.java
+++ src/main/java/com/team2/mbti/book/model/BookServiceImpl.java
@@ -1,5 +1,7 @@
 package com.team2.mbti.book.model;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -8,5 +10,20 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class BookServiceImpl implements BookService {
 	private final BookDAO bookDao;
+
+	@Override
+	public List<BookVO> selectBookAll(BookVO vo) {
+		return bookDao.selectBookAll(vo);
+	}
+
+	@Override
+	public int selectBookCnt() {
+		return bookDao.selectBookCnt();
+	}
+
+	@Override
+	public List<BookVO> selectByConditions(BookVO vo) {
+		return bookDao.selectByConditions(vo);
+	}
 	
 }//
--- src/main/java/com/team2/mbti/book/model/BookVO.java
+++ src/main/java/com/team2/mbti/book/model/BookVO.java
@@ -1,5 +1,35 @@
 package com.team2.mbti.book.model;
 
-public class BookVO {
+import com.team2.mbti.common.SearchVO;
 
-}
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BookVO extends SearchVO{
+	private int bookNo;//BOOK_NO	NUMBER
+	private String bookTitle;//BOOK_TITLE	VARCHAR2(300 BYTE)
+	private int bookPrice;//BOOK_PRICE	NUMBER
+	private String bookPublisher;//BOOK_PUBLISHER	VARCHAR2(50 BYTE)
+	private String bookRegdate;//BOOK_REGDATE	DATE
+	private String bookImgName;//BOOK_IMG_NAME	VARCHAR2(500 BYTE)
+	private int bookImgSize;//BOOK_IMG_SIZE	NUMBER
+	private String bookImgOriginalname;//BOOK_IMG_ORIGINALNAME	VARCHAR2(30 BYTE)
+	private String bookCategory;//BOOK_CATEGORY	VARCHAR2(50 BYTE)
+	private String bookWriter;//BOOK_WRITER	VARCHAR2(50 BYTE)
+	private String bookUseflag;//BOOK_USEFLAG	CHAR(1 BYTE)
+	private String bookExplains;//BOOK_EXPLAINS	VARCHAR2(200 BYTE)
+	private String bookDetails;//BOOK_DETAILS	CLOB
+	
+	@Override
+	public String toString() {
+		return "BookVO [bookNo=" + bookNo + ", bookTitle=" + bookTitle + ", bookPrice=" + bookPrice + ", bookPublisher="
+				+ bookPublisher + ", bookRegdate=" + bookRegdate + ", bookImgName=" + bookImgName + ", bookImgSize="
+				+ bookImgSize + ", bookImgOriginalname=" + bookImgOriginalname + ", bookCategory=" + bookCategory
+				+ ", bookWriter=" + bookWriter + ", bookUseflag=" + bookUseflag + ", bookExplains=" + bookExplains
+				+ ", bookDetails=" + bookDetails + ", toString()=" + super.toString() + "]";
+	}
+	
+	
+}//
