afbef65c405c0a3bad7b4326fdd79052ae72b588
USER <USER@DESKTOP-FU7J2QF>
2023-08-23 23:45:11 +0900

    안쓰는거 삭제~

--- src/main/webapp/js/member.js
+++ src/main/webapp/js/member.js
@@ -1,46 +1,4 @@
-	var contextPath = "/herb";
-
- 	$(function() {	
-		$("#email2").change(function() {
-			if ($("#email2").val() == "etc") {
-				$("#email3").val("");
-				$("#email3").css("visibility", "visible");
-				$("#email3").focus();
-			} else {
-				$("#email3").css("visibility", "hidden");
-			}
-		});
-
-		$('#btnChkId').click(function(){
-	    	var id=$('#userid').val();
-	        open(contextPath+'/member/checkId?userid='+id,'dup',
-	        	'width=590,height=300,location=yes,resizable=yes,left=0,top=0');
-	    });
-		
-		$('#btAdminChkId').click(function(){
-	    	var id=$('#userid').val();
-	        open(contextPath+'/admin/manager/adminCheckId?userid='+id,'dup',
-	        	'width=590,height=300,location=yes,resizable=yes,left=0,top=0');
-	    });
-
-		$('#btnZipcode').click(function(){            
-			window.open(contextPath+'/zipcode/zipcode','zipcode',
-				'width=750,height=800,location=yes,resizable=yes,left=0,top=0');
-	    });
-		
-	});
-
-	function validate_hp(ph) {
-		var pattern = new RegExp(/^[0-9]*$/g);
-		return pattern.test(ph); 
-
-	}
-
-	function validate_userid(uid) {
-		var pattern = new RegExp(/^[a-zA-Z0-9_]+$/g);
-		return pattern.test(uid);
-
-	}
+	var contextPath = "/mbti";
 	
     //본 예제에서는 도로명 주소 표기 방식에 대한 법령에 따라, 내려오는 데이터를 조합하여 올바른 주소를 구성하는 방법을 설명합니다.
     function sample4_execDaumPostcode() {
