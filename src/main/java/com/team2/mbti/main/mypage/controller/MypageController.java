327d43802e516bbc899874871933661cefe05298
pjm0209 <137860775+pjm0209@users.noreply.github.com>
2023-08-29 17:50:24 +0900

    mypage 인클루드화, mypage컨트롤러 생성 by pjm

---
+++ src/main/java/com/team2/mbti/main/mypage/controller/MypageController.java
@@ -0,0 +1,19 @@
+package com.team2.mbti.main.mypage.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/main/mypage")
+public class MypageController {
+	private static final Logger logger = LoggerFactory.getLogger(MypageController.class);
+	
+	@RequestMapping("/mbtiResult")
+	public String mbtiResult() {
+		logger.info("마이페이지 - 나의 mbti 결과 페이지");
+		
+		return "main/mypage/mbtiResult";
+	}
+}
