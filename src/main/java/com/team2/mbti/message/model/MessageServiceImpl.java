f60ebb63ceed8cb95d33ca4c45697f3ce113b79b
pjm0209 <137860775+pjm0209@users.noreply.github.com>
2023-08-30 21:05:39 +0900

    푸푸푸푸쉬 쪽지를 푸쉬해

--- src/main/java/com/team2/mbti/message/model/MessageServiceImpl.java
+++ src/main/java/com/team2/mbti/message/model/MessageServiceImpl.java
@@ -1,6 +1,10 @@
 package com.team2.mbti.message.model;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.team2.mbti.member.model.MemberVO;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,6 +16,11 @@ public class MessageServiceImpl implements MessageService {
 	@Override
 	public int insertSendDm(SendDmVO sendDmVo) {
 		return messageDao.insertSendDm(sendDmVo);
+	}
+
+	@Override
+	public List<MemberVO> selectAllMemberbyDm() {
+		return messageDao.selectAllMemberbyDm();
 	}
 	
 }
