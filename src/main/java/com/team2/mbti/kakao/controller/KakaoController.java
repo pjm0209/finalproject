3910d40594f1159bdeb004a8e6a9657971358e84
yongj <yongj@BOOK-N4C529S7PM>
2023-09-05 10:15:36 +0900

    Merge branch 'main' of https://github.com/pjm0209/finalproject.git into YJ

--- src/main/java/com/team2/mbti/kakao/controller/KakaoController.java
+++ src/main/java/com/team2/mbti/kakao/controller/KakaoController.java
@@ -8,13 +8,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.team2.mbti.kakao.model.KakaoService;
 import com.team2.mbti.member.model.MemberService;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 
 @Controller
-@RequestMapping("/login")
+@RequestMapping("/oauth")
 @RequiredArgsConstructor
 public class KakaoController{
 
@@ -24,19 +26,22 @@ public class KakaoController{
 	private final KakaoService kakaoService;
 	
 	@RequestMapping("/")
-	public String kakaoLogin(HttpSession session, @RequestParam("code") String code) {
+	public String kakaoLogin(@RequestParam("code") String code, HttpServletRequest request, HttpSession session) {
 		logger.info("카카오 로그인 처리, code={}", code);
 		
-		String accessToken = getKakaoAccessToken(code);
-		session.setAttribute("access_token", access_Token); //로그아웃 할때 사용 
+		String access_Token = kakaoService.getKakaoAccessToken(code);
+		logger.info("토큰 받아오기 access_Token={}", access_Token);
+		
+		session.setAttribute("access_token", access_Token); //로그아웃 할때 사용
+		logger.info("로그아웃 처리 access_Token={}", access_Token);
 				
-		HashMap<String, Object> userInfo = KakaoService.getUserInfo(access_Token);
+		HashMap<String, Object> userInfo = kakaoService.getUserInfo(access_Token);
+		logger.info("카카오 회원 정보 불러오기 userInfo={}", userInfo);
 		
-		
-		
-
-		return "";
+		return "redirect:/";
 		
 	}
+
+
 }
 
