a05f675f617675f0be163684e8262320dbba5375
Migi0112 <mingi52020@gmail.com>
2023-09-07 10:36:08 +0900

    1

--- src/main/java/com/team2/mbti/main/controller/MainController.java
+++ src/main/java/com/team2/mbti/main/controller/MainController.java
@@ -1,15 +1,36 @@
 package com.team2.mbti.main.controller;
 
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.team2.mbti.board.model.BoardService;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequestMapping("/main")
+@RequiredArgsConstructor
 public class MainController {
+	private static final Logger logger = LoggerFactory.getLogger(MainController.class);
+	
+	private final BoardService boardService;
 	
 	@GetMapping("/index")
-	public String index_get() {
+	public String index_get(Model model) {
+		logger.info("메인화면 출력");
+		
+		List<Map<String, Object>> faqList = boardService.selectFaqList();
+		
+		model.addAttribute("faqList", faqList);
+		
 		return "main/index";
 	}
 	
