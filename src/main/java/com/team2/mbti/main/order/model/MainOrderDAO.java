237fc59018c94f103d2dcd28e15c12e2f0af2f45
happyplaid <kingdom1723@naver.com>
2023-09-07 17:22:31 +0900

    주문 완료??yjk

--- src/main/java/com/team2/mbti/main/order/model/MainOrderDAO.java
+++ src/main/java/com/team2/mbti/main/order/model/MainOrderDAO.java
@@ -1,5 +1,12 @@
 package com.team2.mbti.main.order.model;
 
-public class MainOrderDAO {
+import org.apache.ibatis.annotations.Mapper;
 
+import com.team2.mbti.main.book.model.MainBookVO;
+
+@Mapper
+public interface MainOrderDAO {
+	int insertOrders(MainOrderVO ovo);
+	int insertOrderDetails(MainBookVO Vo);
+	int insertSales(MainBookVO Vo);
 }
